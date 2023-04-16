d87ba153e700e5367e934ee46aab306ed37ee5ef
afrangyan <afrangyan@kameleoon.com>
2023-04-16 18:11:17 +0300

    created text field input

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './sass/app.sass';
+import React from 'react';
+import TextField from './UI/TextField';
+
+function App() {
+  return (
+    <div className="App">
+      <TextField placeholderText="" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/UI/TextField/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './TextField.module.sass';
+
+const TextField: React.FC<{ placeholderText: string }> = ({ placeholderText }) => {
+  return <input className={styles.root} placeholder={placeholderText} type="text" />;
+};
+
+export default TextField;
